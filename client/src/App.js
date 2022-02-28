193deca78444eb49ce683e43827b1d52a4a4289f
rprice000 <rtprice21@gmail.com>
2022-02-28 13:25:57 -0600

    setup initial imports on react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
-
+// Imports from Pages folder
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 function App() {
   return (
