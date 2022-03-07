5221fbc6c5f2fd1f4e733757631cb81854ee9c69
rprice000 <rtprice21@gmail.com>
2022-03-07 12:59:25 -0600

    nav part 2

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,1 +1,28 @@
-// NAV component page
+import React from 'react';
+import Header from '../components/header';
+
+function Nav(props) {
+    const tabs = [ 'About', 'Portfolio', 'Resume', 'Contact'];
+
+    return(
+        <div>
+            <Header />
+            <ul>
+                {tabs.map((tab) => (
+                    <li key={tab}>
+                        <a 
+                            href={"#" + tab.toLowerCase()}
+                            onclick={() => props.handlePageChange(tab)}
+                            className = {props.currentPage === tab ? "nav-link active" : "nav-link"}
+                        >
+                            {tab}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+
+export default Nav;
