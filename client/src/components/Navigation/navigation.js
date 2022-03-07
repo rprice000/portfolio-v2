8a09d85f66d9740f07a295d94248d05335fbc0b9
rprice000 <rtprice21@gmail.com>
2022-03-07 13:10:02 -0600

    nav part 4

---
+++ client/src/components/Navigation/navigation.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import NavTabs from './index';
+// Will need to add other pages here
+
+
+
+
+
+
+function NavigatePortfolio () {
+    const [currentPage, handlePageChange] = useState('About');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'About': return (<About />);
+            case 'Portfolio': return (<Portfolio />);
+            case 'Resume': return (<Resume />);
+            case 'Contact': return (<Contact />);
+            default: return (<About />);
+        }
+    };
+
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            <div>
+                {
+                    renderPage()
+                }
+            </div>
+        </div>
+    )
+}
+
+export default NavigatePortfolio;
