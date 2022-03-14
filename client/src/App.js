28533b583637462c90dc85b82369e7cde4201db8
rprice000 <rtprice21@gmail.com>
2022-03-14 11:40:29 -0500

    fixed navigation issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-// Imports from Pages folder
-import NavigatePortfolio from "./components/Navigation/navigation";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-function App() {
+// Imports from Pages folder
+// import NavigatePortfolio from './components/Navigation/navigation';
+import About from './pages/About/About';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+import Contact from './pages/Contact/Contact';
+
+const App = () => {
   return (
-    <div>
-      <NavigatePortfolio></NavigatePortfolio>
-    </div>
+    <Router>
+      {/* <div>
+        <NavigatePortfolio></NavigatePortfolio>
+      </div> */}
+      <main>
+        <Routes>
+          <Route exact path="/" element={<About />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
+          <Route exact path="/resume" element={<Resume />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
