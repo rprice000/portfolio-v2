ac2f3cc6785060ac3f4392cc8e345a9f80a3b767
rprice000 <rtprice21@gmail.com>
2022-03-17 18:14:29 -0500

    added Resume content

--- client/src/pages/Resume/Resume.js
+++ client/src/pages/Resume/Resume.js
@@ -1,11 +1,11 @@
 // This is the resume page
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container, Card, Col, Row } from 'react-bootstrap';
 import '../Resume/Resume.css'
 
 const Resume = () => {
     return (
-        <div>
+        <div id="resume-page">
             <Carousel>
                 <Carousel.Item interval={10000}>
                     <img className="d-block w-100" src="images/image-one.jpg" alt="First slide" />
@@ -30,7 +30,78 @@ const Resume = () => {
                 </Carousel.Item>
             </Carousel>
 
+            <Container fluid>
             <h1>Resume</h1>
+
+{/* Education Card */}
+                <Row>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            <Card.Title>Education</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                </Row>
+{/* Technologies & Skills Card */}
+                <Row>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            <Card.Title>Technologies & Skills</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                </Row>
+{/* Professional Experience Section */}
+                <Row>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            <Card.Title>Professional Experience</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                </Row>
+
+
+            </Container>
+
+
+            
         </div>
     )
 }
