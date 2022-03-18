4c0923135cad87ac639eea429ddfe1c218cf027a
rprice000 <rtprice21@gmail.com>
2022-03-18 12:31:49 -0500

    setup resume lists

--- client/src/pages/Resume/Resume.js
+++ client/src/pages/Resume/Resume.js
@@ -1,6 +1,6 @@
 // This is the resume page
 import React from 'react';
-import { Carousel, Container, Card, Col, Row } from 'react-bootstrap';
+import { Carousel, Container, Card, Col, Row, ListGroup } from 'react-bootstrap';
 import '../Resume/Resume.css'
 
 const Resume = () => {
@@ -31,7 +31,6 @@ const Resume = () => {
             </Carousel>
 
             <Container fluid>
-            <h1>Resume</h1>
 
 {/* Education Card */}
                 <Row>
@@ -39,13 +38,29 @@ const Resume = () => {
                    {/* Spacing Column */}
                 </Col>
                 <Col lg="10">
+                <h1 className="text-center">Education</h1>
                     <Card className="shadow-lg p-3 mb-5 bg-body rounded">
                         {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                         <Card.Body>
-                            <Card.Title>Education</Card.Title>
+                            {/* <Card.Title>Education</Card.Title> */}
                             <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
+                                
+                                <ul>
+                                
+                                    <li>Sam Houston State University</li>
+                                        <ul>
+                                            <li>Degree: Bachelor of Business Administration (BBA)</li>
+                                            <li>Completed: May 2010</li>
+                                        </ul>
+
+                                    <li>University of Houston-Victoria</li>
+                                        <ul>
+                                            <li>Degree: Masters in Entrepreneurship and Economic Development (MBA)</li>
+                                            <li>Completed: December 2021</li>
+                                        </ul>
+                                
+                                </ul>
+
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -54,16 +69,113 @@ const Resume = () => {
                     {/* Spacing Column */}
                 </Col>
                 </Row>
-{/* Technologies & Skills Card */}
+
+{/* Education Card */}
+<Row>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                <h1 className="text-center">Education</h1>
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            {/* <Card.Title>Education</Card.Title> */}
+                            <Card.Text>
+                                
+                                
+                                <div className="justify-content-between">
+                                   
+                                    <div>Sam Houston State University</div>
+                                        <div>
+                                            <div>Degree: Bachelor of Business Administration (BBA)</div>
+                                            <div>Completed: May 2010</div>
+                                        </div>
+                                  
+                                    <div>University of Houston-Victoria</div>
+                                        <div>
+                                            <div>Degree: Masters in Entrepreneurship and Economic Development (MBA)</div>
+                                            <div>Completed: December 2021</div>
+                                        </div>
+                                   
+                                </div>
+                              
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                </Row>
+
+{/* Education Card */}
+<Row>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                <h1 className="text-center">Education</h1>
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            {/* <Card.Title>Education</Card.Title> */}
+                            <Card.Text>
+                                
+                                
+                            <ListGroup horizontal className="justify-content-around">
+                                <ListGroup.Item>Sam Houston State University</ListGroup.Item>
+                                <ListGroup.Item>University of Houston-Victoria</ListGroup.Item>
+                            </ListGroup>
+                                                            
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                </Row>
+
+{/* Credentials Card */}
+                <Row>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                <h1 className="text-center">Credentials</h1>
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            {/* <Card.Title>Credentials</Card.Title> */}
+                            <Card.Text>
+                          
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                </Row>
+
+
+
+
+{/* Technologies Card */}
                 <Row>
                 <Col lg="1">
                     {/* Spacing Column */}
                 </Col>
                 <Col lg="10">
+                <h1 className="text-center">Technologies</h1>
                     <Card className="shadow-lg p-3 mb-5 bg-body rounded">
                     {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                         <Card.Body>
-                            <Card.Title>Technologies & Skills</Card.Title>
+                            {/* <Card.Title>Technologies</Card.Title> */}
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
@@ -75,16 +187,41 @@ const Resume = () => {
                    {/* Spacing Column */}
                 </Col>
                 </Row>
+
+{/*Skills Card */}
+                <Row>
+                <Col lg="1">
+                    {/* Spacing Column */}
+                </Col>
+                <Col lg="10">
+                <h1 className="text-center">Skills</h1>
+                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            {/* <Card.Title>Technologies & Skills</Card.Title> */}
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="1">
+                   {/* Spacing Column */}
+                </Col>
+                </Row>
+
 {/* Professional Experience Section */}
                 <Row>
                 <Col lg="1">
                     {/* Spacing Column */}
                 </Col>
                 <Col lg="10">
+                <h1 className="text-center">Professional Experience</h1>
                     <Card className="shadow-lg p-3 mb-5 bg-body rounded">
                 {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                         <Card.Body>
-                            <Card.Title>Professional Experience</Card.Title>
+                            {/* <Card.Title>Professional Experience</Card.Title> */}
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
