a61a8f624dfcb9da868d41025e13d9db0fc5d640
rprice000 <rtprice21@gmail.com>
2022-03-21 13:37:52 -0500

    completed resume styling for credentials and technologies

--- client/src/pages/Resume/Resume.js
+++ client/src/pages/Resume/Resume.js
@@ -1,7 +1,12 @@
 // This is the resume page
 import React from 'react';
-import { Carousel, Container, Card, Col, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Carousel, Container, Card, Col, Row, ListGroup } from 'react-bootstrap';
 import '../Resume/Resume.css'
+
+import Table from 'react-bootstrap/Table'
+
+
+
 
 const Resume = () => {
     return (
@@ -63,10 +68,10 @@ const Resume = () => {
                 <Col lg="1">
                     {/* Spacing Column */}
                 </Col>
-                </Row>
+            </Row>
 
 {/* Credentials Card */}
-                <Row>
+<Row>
                 <Col lg="1">
                    {/* Spacing Column */}
                 </Col>
@@ -75,34 +80,104 @@ const Resume = () => {
                     <Card className="shadow-lg p-3 mb-5 bg-body rounded">
                         <Card.Body>
                             <Card.Text>
-                            <ListGroup>
-                                <Row fluid>
-                                <Col  lg="4" xs="12">
-                                <ListGroup.Item className="text-center">Rice University Full Stack Web Developer Coding Bootcamp – February 2022</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Coursera Advanced Styling and Responsive Design – February 2020</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Coursera Interactivity with JavaScript – February 2020</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Coursera Intro to CSS3 – February 2020</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Coursera Intro to HTML5 – January 2020</ListGroup.Item>
-                                </Col>
-                                <Col lg="4" xs="12">
-                                <ListGroup.Item className="text-center">Coursera Intro to Search Engine Optimization – June 2018</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Cisco CCNA Routing and Switching – March 2010</ListGroup.Item>
-                                <ListGroup.Item className="text-center">N+ Certified – October 2013</ListGroup.Item>
-                                <ListGroup.Item className="text-center">A+ Certified – May 2011 – Updated October 2013</ListGroup.Item>
-                                </Col>
-                                <Col lg="4" xs="12">
-                                <ListGroup.Item className="text-center">HP Certification/ H08-Servicing HP Desktops and Notebooks - September 2005</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Cisco/ Introduction to Telepresence Training Certificate of Completion – February 2012 </ListGroup.Item>
-                                <ListGroup.Item className="text-center">Dell Desktop Certified – May 2011</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Dell Laptop Certified – May 2011</ListGroup.Item>
-                                <ListGroup.Item className="text-center">Dell Network Printer Certified – October 2011</ListGroup.Item>
-                                </Col>
-                                </Row>
-                            </ListGroup>
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Issuing Organization</th>
+                                            <th>Certificate</th>
+                                            <th>Completed</th>
+                                        </tr>
 
+                                    </thead>
 
+                                    <tbody>
 
-                             
+                                        <tr>
+                                            <td>Rice University</td>
+                                            <td>Full Stack Web Developer Coding Bootcamp</td>
+                                            <td>February 2022</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Coursera</td>
+                                            <td>Advanced Styling and Responsive Design</td>
+                                            <td>February 2020</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Coursera</td>
+                                            <td>Interactivity with JavaScript</td>
+                                            <td>February 2020</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Coursera</td>
+                                            <td>Intro to CSS3</td>
+                                            <td>February 2020</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Coursera</td>
+                                            <td>Intro to HTML5</td>
+                                            <td>January 2020</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Coursera</td>
+                                            <td>Intro to Search Engine Optimization</td>
+                                            <td>June 2018</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Cisco Systems</td>
+                                            <td>CCNA Routing and Switching</td>
+                                            <td>March 2010</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>CompTIA</td>
+                                            <td>N+ Certified </td>
+                                            <td>October 2013</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>CompTIA</td>
+                                            <td>A+ Certified</td>
+                                            <td>May 2011</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Cisco Systems</td>
+                                            <td>Introduction to Telepresence Training Certificate of Completion</td>
+                                            <td>February 2012</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Dell</td>
+                                            <td>Desktop Certified </td>
+                                            <td>May 2011</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Dell</td>
+                                            <td>Laptop Certified</td>
+                                            <td>May 2011</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Dell</td>
+                                            <td>Network Printer Certified</td>
+                                            <td>October 2011</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>HP/Hewlett-Packard</td>
+                                            <td>H08-Servicing HP Desktops, Workstations, and Notebooks</td>
+                                            <td>September 2010</td>
+                                        </tr>
+                                       
+                                    </tbody>
+                                </Table>
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -111,7 +186,6 @@ const Resume = () => {
                     {/* Spacing Column */}
                 </Col>
                 </Row>
-
 
 
 
@@ -127,8 +201,44 @@ const Resume = () => {
                         <Card.Body>
                             {/* <Card.Title>Technologies</Card.Title> */}
                             <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
+                            <ListGroup>
+                                <Row fluid>
+                                    <Col  lg="4" xs="12">
+                                        <ListGroup.Item className="text-center">Visual Studio</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Git</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Mongo Atlas</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">MySQL Workbench</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Insomnia API Client</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">HTML5</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">CSS3</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">BootStrap</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Bulma</ListGroup.Item>
+                                    </Col>
+                                    <Col lg="4" xs="12">
+                                        <ListGroup.Item className="text-center">Semantic UI</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">JavaScript</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Node.js</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Express.js</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">SQL</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">NoSQL</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">React.js</ListGroup.Item>
+                                    </Col>
+                                    <Col lg="4" xs="12">
+                                        <ListGroup.Item className="text-center">GraphQL</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">MongoDB</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Apollo Server</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Stripe Payment</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Redux</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Object Oriented Programming (OOP)</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Object Relational Mapping (ORM)</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Model View Controller (MVC)</ListGroup.Item>
+                                        <ListGroup.Item className="text-center">Progressive Web Application (PWA) </ListGroup.Item>
+                                    </Col>
+                                </Row>
+                            </ListGroup>
+                        
+            
+
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -138,28 +248,9 @@ const Resume = () => {
                 </Col>
                 </Row>
 
-{/*Skills Card */}
-                <Row>
-                <Col lg="1">
-                    {/* Spacing Column */}
-                </Col>
-                <Col lg="10">
-                <h1 className="text-center">Skills</h1>
-                    <Card className="shadow-lg p-3 mb-5 bg-body rounded">
-                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                        <Card.Body>
-                            {/* <Card.Title>Technologies & Skills</Card.Title> */}
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col lg="1">
-                   {/* Spacing Column */}
-                </Col>
-                </Row>
+
+
+
 
 {/* Professional Experience Section */}
                 <Row>
