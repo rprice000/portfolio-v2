0682b7e3600f5496f05d621692f4a4b6fb22c209
rprice000 <rtprice21@gmail.com>
2022-03-23 11:14:39 -0500

    added styling to carousel

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,6 +1,6 @@
 // React Imports
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Row, Col } from 'react-bootstrap';
 // Component and CSS Imports
 import './Slider.css'
 
@@ -9,46 +9,36 @@ const Slider = () => {
 
 
     return (
-            <Carousel id="slider">
-                <Carousel.Item>
-                    <img
-                    id="slide-1"
-                    className="d-block w-100"
-                    src="../../images/image-one.jpg"
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    id="slide-2"
-                    className="d-block w-100"
-                    src="../../images/image-one.jpg"
-                    alt="Second slide"
-                    />
 
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
+            <Carousel fade 
+            // controls={false} 
+            // indicators={false} 
+            id="slider">
+                
                 <Carousel.Item>
-                    <img
-                    id="slide-3"
-                    className="d-block w-100"
-                    src="../../images/image-one.jpg"
-                    alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                    <Row>
+                        <Col className="d-flex justify-content-center align-items-center">
+                            <h1>Rice University Coding BootCamp</h1>
+                        </Col>
+             
+                        <Col>
+                        </Col>
+                    </Row>
                 </Carousel.Item>
+
+                <Carousel.Item>
+                <h1>Item 2</h1>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <h1>Item 3</h1>
+                </Carousel.Item>
+
             </Carousel>
+
+
+
+
     )
 }
 
