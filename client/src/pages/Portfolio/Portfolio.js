28292ef801aa0834098ad77799846ee04a9a73fd
rprice000 <rtprice21@gmail.com>
2022-03-24 13:55:00 -0500

    more testing on portfolio page part 6

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 // React Imports
 import React from 'react';
 // import { Container, Card, Col, Row, ListGroup, Table } from 'react-bootstrap';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 
 // Styling Imports
 import '../Portfolio/Portfolio.css'
@@ -20,55 +20,55 @@ const Portfolio = () => {
                 {/* d-flex justify-content-center align-items-center */}
                     <Row>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>1</h3>
-                            </Card>
+                            </div>
                         </Col>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>2</h3>
-                            </Card>
+                            </div>
                         </Col>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>3</h3>
-                            </Card>
+                            </div>
                         </Col>
                     </Row>
 
                     <Row>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>4</h3>
-                            </Card>
+                            </div>
                         </Col>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>5</h3>
-                            </Card>
+                            </div>
                         </Col>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>6</h3>
-                            </Card>
+                            </div>
                         </Col>
                     </Row>
 
                     <Row className="justify-content-around">
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>7</h3>
-                            </Card>
+                            </div>
                         </Col>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>8</h3>
-                            </Card>
+                            </div>
                         </Col>
                         <Col className="card-column d-flex justify-content-center">
-                            <Card className="cards">
+                            <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>9</h3>
-                            </Card>
+                            </div>
                         </Col>
                     </Row>
 
