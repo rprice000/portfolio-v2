6493caa263a118b14b83b98fb5ebf3b90fbd73a1
rprice000 <rtprice21@gmail.com>
2022-03-24 13:37:22 -0500

    more testing on portfolio page

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -1,64 +1,83 @@
 // React Imports
 import React from 'react';
 // import { Container, Card, Col, Row, ListGroup, Table } from 'react-bootstrap';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 // Styling Imports
 import '../Portfolio/Portfolio.css'
-// import '../Portfolio/Portfolio.scss'
+
 // Component Imports
 
 
 
 const Portfolio = () => {
     return (
-                    <div class="portfolio-body" >
-                        <Container>
-                            <Row>
-                            <div class="card">
-                            <div class="face face1">
-                                <div class="content">
-                                <h2 class="java">Java</h2>
-                                <p class="java">Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.</p>
-                                </div>
-                            </div>
-                            <div class="face face2">
-                                <h2>01</h2>
-                            </div>
-                            </div>
+            <div id="portfolio-page">
+                <section id="portfolio-hero">
+                <h1>Portfolio</h1>
+                </section>
+                <Container fluid>
 
-                            <div class="card">
-                            <div class="face face1">
-                                <div class="content">
-                                <h2 class="python">Python</h2>
-                                <p class="python">Python is an interpreted, high-level and general-purpose programming language.</p>
-                                </div>
-                            </div>
-                            <div class="face face2">
-                                <h2>02</h2>
-                            </div>
-                            </div>
+                    <Row>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>1</h3>
+                            </Card>
+                        </Col>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>2</h3>
+                            </Card>
+                        </Col>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>3</h3>
+                            </Card>
+                        </Col>
+                    </Row>
 
-                            <div class="card">
-                            <div class="face face1">
-                                <div class="content">
-                                <h2 class="cSharp">C#</h2>
-                                <p class="cSharp">C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped and component-oriented programming disciplines.</p>
-                                </div>
-                            </div>
-                            <div class="face face2">
-                                <h2>03</h2>
-                            </div>
-                            </div>
-                            </Row>
+                    <Row>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>4</h3>
+                            </Card>
+                        </Col>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>5</h3>
+                            </Card>
+                        </Col>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>6</h3>
+                            </Card>
+                        </Col>
+                    </Row>
 
-                            
+                    <Row className="justify-content-around">
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>7</h3>
+                            </Card>
+                        </Col>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>8</h3>
+                            </Card>
+                        </Col>
+                        <Col className="card-column">
+                            <Card className="cards">
+                                <h3>9</h3>
+                            </Card>
+                        </Col>
+                    </Row>
 
 
-                        </Container>
 
-                    </div>
 
+
+                </Container>
+            </div>    
 
     )
 }
