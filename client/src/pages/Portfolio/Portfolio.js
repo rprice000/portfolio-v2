ee4cade160d11cbba3aaae628c584559ec75002e
rprice000 <rtprice21@gmail.com>
2022-03-25 09:17:11 -0500

    styling cards in portfolio part 8

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 // React Imports
-import React from 'react';
+import React, { useState } from 'react';
 // import { Container, Card, Col, Row, ListGroup, Table } from 'react-bootstrap';
-import { Container, Row, Col, Button, Nav} from 'react-bootstrap';
+import { Container, Row, Col, Button, Nav, Modal} from 'react-bootstrap';
 
 // Styling Imports
 import '../Portfolio/Portfolio.css'
@@ -11,6 +11,13 @@ import '../Portfolio/Portfolio.css'
 
 
 const Portfolio = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
             <div id="portfolio-page">
                 <section id="portfolio-hero">
@@ -25,9 +32,21 @@ const Portfolio = () => {
                             <div className="cards d-flex">
                                 <div class="card-content-spacing card-bottom">
                                     <Nav className="flex-column">
-                                        <Button className="mt-2 mb-2">Deployed App</Button>
-                                        <Button className="mt-2 mb-2">GitHub Repo</Button>
-                                        <Button className="mt-2 mb-2">Modal Info</Button>
+                                        <Button href="https://rprice000.github.io/run-buddy/" target="_blank" className="mt-2 mb-2">Deployed App</Button>
+                                        <Button href="https://github.com/rprice000/run-buddy" target="_blank" className="mt-2 mb-2">GitHub Repo</Button>
+                                        <Button className="mt-2 mb-2" onClick={handleShow}>Modal Info</Button>
+
+                                        <Modal show={show} onHide={handleClose}>
+                                            <Modal.Header>
+                                            <Modal.Title>Modal heading</Modal.Title>
+                                            </Modal.Header>
+                                            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                                            <Modal.Footer>
+                                            <Button variant="secondary" onClick={handleClose}>
+                                                Close
+                                            </Button>
+                                            </Modal.Footer>
+                                        </Modal>
                                     </Nav>
                                 </div>
 
@@ -46,6 +65,8 @@ const Portfolio = () => {
                                 <h3>2</h3>
                             </div>
                         </Col>
+
+
                         <Col className="card-column d-flex justify-content-center">
                             <div className="cards d-flex justify-content-center align-items-center">
                                 <h3>3</h3>
