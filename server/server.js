5cb73358c7ce90e379297156cbf00c38b70f4f9f
rprice000 <rtprice21@gmail.com>
2022-03-25 13:49:46 -0500

    testing deployment

--- server/server.js
+++ server/server.js
@@ -3,9 +3,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'client/public');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5001;
 
 app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
@@ -17,4 +23,4 @@ app.get('*', (req, res) => {
     console.log('Server is up!');
  });
 
-//  http://localhost:3001
+//  http://localhost:5001
